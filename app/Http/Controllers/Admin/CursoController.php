f20a45f5caecb6144e92a079593fbe51948c9db7
Lucas Augusto <lucasaugustoq@gmail.com>
2023-01-19 13:45:47 -0300

    Configurado Meterialize

--- app/Http/Controllers/Admin/CursoController.php
+++ app/Http/Controllers/Admin/CursoController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Curso;
 
 class CursoController extends Controller
 {
     public function index () 
     {
-        return view('admin\cursos\index');
+        $registros = Curso::all();
+        return view('admin\cursos\index',compact('registros'));
     }
         
 }
--- resources/views/admin/cursos/index.blade.php
+++ resources/views/admin/cursos/index.blade.php
@@ -1,1 +1,41 @@
-"essa é a view index"
+@extends('template')
+
+@section('titulo','CURSOS')
+
+@section('conteudo')
+    <div class="container">
+        <h3 class="center">Lista de cursos</h3>
+        <div class="row">
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Título</th>
+                        <th>Descrição</th>
+                        <th>Imagem</th>
+                        <th>Publicado</th>
+                        <th>Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($registros as $registro)
+                        <tr>
+                            <td>{{$registro->id}}</td>
+                            <td>{{$registro->titulo}}</td>
+                            <td>{{$registro->descrição}}</td>
+                            <td>img width="120" src="{{asset($registro->imagem)}}" alt="{{$registro->titulo}}"</td>
+                            <td>{{$registro->publicado}}</td>
+                            <td>
+                                <a class="btn deep-orange" href="{{route('admin.cursos.editar',$registro->id)}}">Editar</a>
+                                <a class="btn red" href="{{route('admin.cursos.deletar',$registro->id)}}">Deletar</a>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="row">
+            <a class="btn blue" href="{{route('admin.cursos.adicionar')}}">Adicionar</a>
+        </div>
+    </div>
+
+@endsection
